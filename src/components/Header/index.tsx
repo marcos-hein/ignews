bf25aaa670b27cf6c87ef0fe7077ff9efe7174a2
Marcos Pereira <marcos.hen-ext@via.com.br>
2021-08-31 11:44:46 -0300

    Criando componente SignInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SigInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
@@ -9,6 +10,7 @@ export function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+        <SigInButton />
       </div>
     </header>
   )
