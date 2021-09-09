0970f628c6ec59585144825b748241122f2b06cd
Marcos V. H. Pereira <60493029+marcos-hein@users.noreply.github.com>
2021-09-08 21:21:02 -0300

    Implementando Next Auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -11,6 +11,13 @@ interface HomeProps {
     amount: number;
   }
 }
+
+// Client-side
+// Server-side
+// Static Site Generation
+
+// Conteúdo (SSG)
+// Comentários (Client-side)
 
 export default function Home({ product }: HomeProps) {
   return (
@@ -38,7 +45,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JVQGaGzbOzYEl1wU3jeopaH');
   const product = {
     priceId: price.id,
@@ -50,7 +57,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: {
-      product
-    }
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,6 @@
 import Stripe from 'stripe';
 import { version } from '../../package.json';
 
-const api = "sk_test_51JVQDoGzbOzYEl1wXYuiNQwzp1E8GtTeKt9FJeD9fiYg1ClUhU4LsGGgPpWqmF04Y5I7gLc9KKww1bpadCnFc49T00TFXNJrG6"
 export const stripe = new Stripe(
   process.env.STRIPE_API_KEY,
   {
