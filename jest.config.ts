b514a8ee044c1c3d85a0df7d9d1e0ed3953bf7f0
Marcos Pereira <marcos.hen-ext@via.com.br>
2022-12-28 09:58:04 -0300

    :wrench: configuring testing library

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  testEnvironment: 'jsdom',
+};
+
+export default config;
