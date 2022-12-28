9988035588dae83806bed6900f806a686d97ecb6
Marcos Pereira <marcos.hen-ext@via.com.br>
2022-12-28 14:23:08 -0300

    :white_check_mark: testing header

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(scss|css|sass)$': 'identity-obj-proxy',
+  },
 };
 
 export default config;
