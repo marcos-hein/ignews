351eec1d8bd1d39abda49295419494b7bcdada6c
Marcos Pereira <marcos.hen-ext@via.com.br>
2022-12-28 15:12:23 -0300

    :white_check_mark: testing SubscribeButton component

---
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -0,0 +1,63 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { SubscribeButton } from '.';
+
+jest.mock('next-auth/client');
+jest.mock('next/router');
+
+describe('SubscribeButton component', () => {
+  it('renders correctly when user is not authenticated', () => {
+    const useSessionMocked = jest.mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SubscribeButton />);
+
+    expect(screen.getByText('Subscribe now')).toBeInTheDocument();
+  });
+
+  it('redirects user to sign in when not authenticated', () => {
+    const useSessionMocked = jest.mocked(useSession);
+    const signInMocked = jest.mocked(signIn);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText('Subscribe now');
+
+    fireEvent.click(subscribeButton);
+
+    expect(signInMocked).toHaveBeenCalled();
+  });
+
+  it('redirects to posts when user has a subscription', () => {
+    const useRouterMocked = jest.mocked(useRouter);
+    const useSessionMocked = jest.mocked(useSession);
+    const pushMock = jest.fn();
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: 'Jhon Doe',
+          email: 'jhon.doe@example.com',
+        },
+        activeSubscription: 'fake-active-subscription',
+        expires: 'fake-expires',
+      },
+      false,
+    ]);
+
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMock,
+    } as any);
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText('Subscribe now');
+
+    fireEvent.click(subscribeButton);
+
+    expect(pushMock).toHaveBeenCalledWith('/posts');
+  });
+});
